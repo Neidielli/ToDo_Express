09d3d2618b6fbf9454340d40ff89347396b95842
Neidielli <neidielli@hotmail.com>
2023-10-03 10:43:41 -0300

    Recebendo o nomeUsuario e adicionando na sessão

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// Configurar a pasta de views e o template engine Mustache
+// Configura a pasta de views e o template engine Mustache
 app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 
-// Servir arquivos estáticos a partir da pasta 'public'
+// Serve os arquivos estáticos a partir da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Configura o body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: 'admin', 
+  resave: false,
+  saveUninitialized: true
+}));
 
 // Rota para a página inicial
 app.get('/', (req, res) => {
-  res.render('index');
+  const nomeUsuario = req.session.nomeUsuario || 'Convidado'; // Obtem o nome da sessão
+  res.render('index', { nomeUsuario });
 });
+
+app.post('/salvauser', (req, res) => {
+  const { nome } = req.body;
+
+  if (nome) {
+    req.session.nomeUsuario = nome; // Armazena o nome do usuário na sessão
+  }
+
+  res.redirect('/'); // Redireciona para a página inicial
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
