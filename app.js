ad75d3d83fd37629951a86be1dc8a33981a6a032
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-10-04 23:46:30 -0300

    2 contadores de requisição

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const mustacheExpress = require('mustache-express');
 const path = require('path');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+let contadorGlobal = 0;
 
 // Configura a pasta de views e o template engine Mustache
 app.engine('mustache', mustacheExpress());
@@ -13,10 +16,18 @@ app.set('view engine', 'mustache');
 
 // Serve os arquivos estáticos a partir da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+// Cookie
+app.use(cookieParser());
 
 // Configura o body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Contagem de todas as requisições
+app.use((req, res, next) => {
+  contadorGlobal++;
+  next();
+});
 
 app.use(session({
   secret: 'admin', 
@@ -39,6 +50,30 @@ app.post('/salvauser', (req, res) => {
   }
 
   res.redirect('/tasks'); // Redireciona para a página inicial
+});
+
+// Rota para gerar um número aleatório e salvar em um cookie
+app.get('/random', (req, res) => {
+  // Verifica se o cookie já existe
+  const randomNumber = req.cookies.randomNumber || Math.floor(Math.random() * 1000);
+
+  // Salva o número aleatório em um cookie com um nome "randomNumber"
+  res.cookie('randomNumber', randomNumber);
+
+  // res.send(`Número aleatório: ${randomNumber}`);
+  res.render('cookie', { randomNumber });
+});
+
+// Rota para /contador 
+app.get('/contador', (req, res) => {
+  // Verifica se o cookie contadorUsuario já existe para o usuário atual
+  const contadorUsuario = req.cookies.contadorUsuario || 0;
+
+  // Incrementa o contador do usuário e salva no cookie
+  res.cookie('contadorUsuario', parseInt(contadorUsuario) + 1);
+
+  // res.send(`Contador Global: ${contadorGlobal}<br>Contador do Usuário: ${contadorUsuario}`);
+  res.render('contador', { contadorGlobal, contadorUsuario});
 });
 
 const router = require('./routes');
